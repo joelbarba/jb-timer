140049e66991fb5221980b6e2f9aab6c156ed416
joel.barba <joel.barba.vidal@gmail.com>
2025-04-14 18:11:49 +0200

    Main layout

---
+++ script.js
@@ -0,0 +1,9 @@
+console.log('Ini');
+
+
+// document.getElementById('big-btn').oncontextmenu = function(event) {
+//   event.preventDefault();
+//   event.stopPropagation();
+//   event.stopImmediatePropagation();
+//   return false;
+// };
